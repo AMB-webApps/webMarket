d396b066dc18a206d88a8cbb416b1c03ffae12b7
abdullo.shukurov <abdullo.shukurov@icloud.com>
2021-12-06 22:44:32 +0500

    :fire:

--- laptops/views.py
+++ laptops/views.py
@@ -1,15 +1,14 @@
+# Django
 from django.http import Http404
-from django.shortcuts import get_object_or_404
-from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
+# Rest-Framework
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-
+# Project
 from .serializers import ProductSerializer
-from rest_framework.generics import CreateAPIView, RetrieveAPIView
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Product
 
 
@@ -48,7 +47,7 @@ class ProductDetailView(APIView):
         return Response(serializer.data)
 
 
-class SnippetUpdateView(APIView):
+class ProductUpdateView(APIView):
 
     def get_object(self, pk):
         try:
@@ -57,8 +56,8 @@ class SnippetUpdateView(APIView):
             raise Http404
 
     def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = ProductSerializer(snippet, data=request.data)
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -74,6 +73,6 @@ class ProductDeleteView(APIView):
             raise Http404
 
     def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
+        product = self.get_object(pk)
+        product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
